e7eac5d7bbc6477f22532c9f6059c406c5f2d60b
David <davidobob@laposte.net>
2020-10-03 11:47:00 +0200

    home components fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/navBar";
 import LoginForm from "./components/loginForm";
 import Home from "./components/home";
 import NewGameForm from "./components/newGameForm";
+import RegisterForm from "./components/registerForm";
+import auth from "./services/authService";
 
 class App extends Component {
   state = {};
@@ -17,19 +19,21 @@ class App extends Component {
   render() {
     const { user } = this.state;
 
-  return (
-    <React.Fragment>
-      <main className="container">
-        <NavBar />
-        <Switch>
-          <Route path="/games/:id/start" component={} />
-          <Route path="/games/new" component={NewGameForm} />
-          <Route path="/login" component={LoginForm} />
-          <Route path="/" component={Home} />
-        </Switch>
-      </main>
-    </React.Fragment>
-  );
+    return (
+      <React.Fragment>
+        <main className="container">
+          <NavBar user={user} />
+          <Switch>
+            {/* <Route path="/games/:id/start" component={} /> */}
+            <Route path="/games/new" component={NewGameForm} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
