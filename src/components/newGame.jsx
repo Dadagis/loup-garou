9840c599e99dbd8df916c440661fabf714abcb75
David <davidobob@laposte.net>
2020-10-03 14:08:38 +0200

    removed pusher

--- src/components/newGame.jsx
+++ src/components/newGame.jsx
@@ -1,23 +1,15 @@
-import React from "react";
-const Pusher = require("pusher");
+import React, { Component } from "react";
 
-export default function NewGame() {
-  var pusher = new Pusher("e3e84ab3de7b94195578", {
-    cluster: "eu",
-  });
-
-  var channel = pusher.subscribe("my-channel");
-  channel.bind("my-event", function (data) {
-    alert(JSON.stringify(data));
-  });
-
-  return (
-    <div>
-      <h1>Nouvelle Partie</h1>
-      <p>Vous êtes le créateur de la partie</p>
+export default class NewGame extends Component {
+  render() {
+    return (
       <div>
-        <h2>Liste des joueurs</h2>
+        <h1>Salon d'avant partie</h1>
+        <p>Vous êtes le créateur de la partie</p>
+        <div>
+          <h2>Liste des joueurs</h2>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
