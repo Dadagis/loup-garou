e72ee0d5d1d1c8ea8e97e48164026cf8fd6ac30c
David <davidobob@laposte.net>
2020-10-03 12:13:34 +0200

    redirection after creating a game

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from "./components/home";
 import NewGameForm from "./components/newGameForm";
 import RegisterForm from "./components/registerForm";
 import auth from "./services/authService";
+import NewGame from "./components/newGame";
 
 class App extends Component {
   state = {};
@@ -24,7 +25,7 @@ class App extends Component {
         <main className="container">
           <NavBar user={user} />
           <Switch>
-            {/* <Route path="/games/:id/start" component={} /> */}
+            <Route path="/games/:id/start" component={NewGame} />
             <Route path="/games/new" component={NewGameForm} />
             <Route path="/login" component={LoginForm} />
             <Route path="/register" component={RegisterForm} />
---
+++ src/components/newGame.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class NewGame extends Component {
+  render() {
+    return (
+      <div>
+        <h1>nouvelle partie</h1>
+      </div>
+    );
+  }
+}
