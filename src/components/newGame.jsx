4199c42bf2900fb129f867d2627a41dcee0387fe
David <davidobob@laposte.net>
2020-10-03 15:56:35 +0200

    added roles section

--- src/components/newGame.jsx
+++ src/components/newGame.jsx
@@ -1,13 +1,55 @@
 import React, { Component } from "react";
+import Axios from "axios";
 
 export default class NewGame extends Component {
+  state = { players: [], roles: [] };
+
+  componentWillMount = async () => {
+    await Axios.get("http://localhost:4000/api/roles/index").then(
+      (response) => {
+        this.setState({ roles: response.data });
+      }
+    );
+  };
+
+  handleClick = async () => {
+    try {
+      const headers = {
+        "x-auth-token":
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZjU4Yzc3OWYxNWYzOTBlYWQ0YWNmN2MiLCJpYXQiOjE1OTk2NTM3NTN9.8DwdfGlEZRz3ZHHDD98XmoYKTTPhDs-gJIG-xVBQZLk",
+      };
+      const gameId = this.props.location.pathname.split("/")[2];
+
+      await Axios.get(`http://localhost:4000/api/games/${gameId}`, {
+        headers: headers,
+      }).then((response) => {
+        this.setState({ players: response.data.players });
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   render() {
+    const { roles } = this.state;
     return (
       <div>
         <h1>Salon d'avant partie</h1>
-        <p>Vous êtes le créateur de la partie</p>
+        <p>Vous êtes le créateur de la partie vous devez choisir les rôles</p>
+        <h2>Rôles</h2>
         <div>
-          <h2>Liste des joueurs</h2>
+          {roles.map((role) => (
+            <span key={role._id}>{role.name}</span>
+          ))}
+        </div>
+        <div>
+          <h3>Liste des joueurs</h3>
+          <ul>
+            {this.state.players.map((player) => (
+              <li key={player}>{player}</li>
+            ))}
+          </ul>
+          <button onClick={this.handleClick}>Update</button>
         </div>
       </div>
     );
