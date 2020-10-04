39f4538629e23523ada43c08f02ff99cc15f0e1c
David <davidobob@laposte.net>
2020-10-04 13:33:21 +0200

    added id to share

--- src/components/newGame.jsx
+++ src/components/newGame.jsx
@@ -3,75 +3,72 @@ import http from "../services/httpService";
 import auth from "../services/authService";
 
 export default class NewGame extends Component {
-  state = { game: {},roles: [], gameRoles: [], interval: "" };
-
+  state = { game: {}, roles: [], gameRoles: [], interval: "" };
 
   componentDidMount = async () => {
     await this.getGame();
     await this.getRoles();
     this.getGameRoles();
     this.refreshGame();
-    
   };
 
-  getGame = async function(){
+  getGame = async function () {
     try {
       const headers = {
-        "x-auth-token":
-          auth.getJwt(),
+        "x-auth-token": auth.getJwt(),
       };
 
       const gameId = this.props.location.pathname.split("/")[2];
 
-      await http.get(`http://localhost:4000/api/games/${gameId}`, 
-      {headers}
-      ).then((response) => {
-        this.setState({ game: response.data });
-      });
-    } catch (error) {
-    }
-  }
-  refreshGame = async function() {
+      await http
+        .get(`http://localhost:4000/api/games/${gameId}`, { headers })
+        .then((response) => {
+          this.setState({ game: response.data });
+        });
+    } catch (error) {}
+  };
+  refreshGame = async function () {
     var interval = setInterval(async () => {
       this.getGame();
     }, 1000);
-    this.setState({interval});
-  }
+    this.setState({ interval });
+  };
 
-  getRoles = async function(){
-    await http.get("http://localhost:4000/api/roles/index").then(
-      (response) => {
-        this.setState({ roles: response.data });
-      }
-    );
-  }
+  getRoles = async function () {
+    await http.get("http://localhost:4000/api/roles/index").then((response) => {
+      this.setState({ roles: response.data });
+    });
+  };
 
-  getGameRoles = function(){
-    const {roles, game} = this.state;
+  getGameRoles = function () {
+    const { roles, game } = this.state;
     let gameRoles = [];
     console.log(game);
-    game.rolesId.forEach(roleId => {
-      roles.forEach(role => {
-        if (roleId === role._id){
+    game.rolesId.forEach((roleId) => {
+      roles.forEach((role) => {
+        if (roleId === role._id) {
           gameRoles.push(role);
         }
       });
     });
-    this.setState({gameRoles})
-  }
-  
-  componentWillUnmount = function() {
+    this.setState({ gameRoles });
+  };
+
+  componentWillUnmount = function () {
     // use intervalId from the state to clear the interval
     clearInterval(this.state.interval);
   };
 
-
   render() {
     const { gameRoles, game } = this.state;
     const { players } = this.state.game;
+    const id = this.props.location.pathname.split("/")[2];
     return (
       <div>
         <h1>Salon d'avant partie</h1>
+        <p>
+          Voici le code à partager avec vos amis : <strong>{id}</strong>
+        </p>
         <p>Vous êtes le créateur de la partie vous devez choisir les rôles</p>
         <h2>Rôles</h2>
         <div>
@@ -81,9 +78,7 @@ export default class NewGame extends Component {
         </div>
         <div>
           <h3>Liste des joueurs</h3>
-          <ul>
-          
-          </ul>
+          <ul></ul>
         </div>
       </div>
     );
